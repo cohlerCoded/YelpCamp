027d37333bf1740173af05deebd7a5514df38bf9
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-12 14:55:18 -0800

    more error handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -38,6 +39,7 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError("Invalid Campground", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -88,8 +90,13 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("PAGE NOT FOUND", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("OH BOY SOMETHING WENT WRONG!");
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
