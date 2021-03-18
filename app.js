8485a671a616a628de79b8658235d9eb04dfaedf
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-18 13:04:13 -0700

    display reviews

--- app.js
+++ app.js
@@ -79,7 +79,10 @@ app.get(
 app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    console.log(campground);
     res.render("campgrounds/details", { campground });
   })
 );
