93f50cfdb579cc046b268048bade7b8e31e35874
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-24 11:55:47 -0700

    made register form

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 const { getMaxListeners } = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -64,13 +65,14 @@ app.use((req, res, next) => {
 });
 
 app.get("/fakeUser", async (req, res) => {
-  const user = new User({ email: "colt@gmail.com", username: "colttt" });
+  const user = new User({ email: "jeff@gmail.com", username: "jefff" });
   const newUser = await User.register(user, "chicken");
   res.send(newUser);
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
