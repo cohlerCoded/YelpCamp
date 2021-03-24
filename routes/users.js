f92b2e5121eca86ba82f86f645c9d9d045d864be
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-24 12:18:58 -0700

    register route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
