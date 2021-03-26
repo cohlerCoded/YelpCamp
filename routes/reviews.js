1ef3ca30cbc03babf984493419eae64a69267bfe
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-26 15:25:56 -0700

    exported review routes to controllers folder

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,33 +5,15 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+const { createReview, deleteReview } = require("../controllers/reviews");
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", `Review Successfully Created!`);
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post("/", isLoggedIn, validateReview, catchAsync(createReview));
+
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", `Review Successfully Deleted!`);
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(deleteReview)
 );
 
 module.exports = router;
